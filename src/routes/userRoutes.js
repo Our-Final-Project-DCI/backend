1eb70384c599768ec016b46806ac89759cc39067
alaa nasser <alaanasser749@gmail.com>
2022-09-15 14:42:15 +0200

    controller für route signup

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,23 @@
-const User = require("../models/User");
-
 const express = require("express");
+
+const controller = require("../controllers/userController");
+const validator = require("../lib/validators/userValidator");
 
 const app = express.Router();
 
-//
+// http://localhost:3007
+
+// 1) /user
+
+//app.get("/", controller.getCurrentUser);
+
+// 2)  /user/signup
+app.post("/signup", validator.signup, controller.signup);
+
+// 3)  /user/login
+//app.post("/login", validator.login, controller.login);
+
+// 4)  /user/logout
+//app.post("/logout", controller.logout);
 
 module.exports = app;
