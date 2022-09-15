a556068e667ab98c40024ec3b7f4e970ab96b849
alaa nasser <alaanasser749@gmail.com>
2022-09-15 15:26:15 +0200

    signup done

--- src/lib/validators/userValidator.js
+++ src/lib/validators/userValidator.js
@@ -5,13 +5,28 @@ const errorsValidator = require("../middlewares/errorsValidator");
 exports.signup = [
   body("username")
     .exists()
-
+    //.isString()
     .isLength({ min: 3 })
     .withMessage("This username must me 3+ characters long "),
   body("email")
     .trim()
     .isEmail()
+    //.notEmpty()
+    //.normalizeEmail()
+    .withMessage("Email is not valid"),
+  body("password")
+    //.isStrongPassword()
+    .isLength({ min: 6 })
+    .withMessage("your Password is not strong und must be +8 characters long"),
+  errorsValidator,
+];
 
+exports.login = [
+  body("email")
+    .trim()
+    .isEmail()
+    .notEmpty()
+    .normalizeEmail()
     .withMessage("Email is not valid"),
   body("password")
     .isStrongPassword()
@@ -19,19 +34,5 @@ exports.signup = [
     .withMessage("your Password is not strong und must be +8 characters long"),
   errorsValidator,
 ];
-
-// exports.login = [
-//   body("email")
-//     .trim()
-//     .isEmail()
-//     .notEmpty()
-//     .normalizeEmail()
-//     .withMessage("Email is not valid"),
-//   body("password")
-//     .isStrongPassword()
-//     .isLength({ min: 8 })
-//     .withMessage("your Password is not strong und must be +8 characters long"),
-//   errorsValidator,
-// ];
 
 //exports.update = [];
