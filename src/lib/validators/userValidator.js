cc552713eeed756cba7f5ee32016704adfbb6206
alaa nasser <alaanasser749@gmail.com>
2022-09-15 12:18:30 +0200

    #17

--- src/lib/validators/userValidator.js
+++ src/lib/validators/userValidator.js
@@ -4,19 +4,29 @@ const errorsValidator = require("../middlewares/errorsValidator");
 
 exports.signup = [
   body("username")
-    .isLength({ min: 2 })
-    .withMessage("username should have min 2 letter "),
-  body("email").isEmail().withMessage("invalid Email improve your Email"),
+    .exists()
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("This username must me 3+ characters long "),
+  body("email")
+    .trim()
+    .isEmail()
+    .notEmpty()
+    .normalizeEmail()
+    .withMessage("Email is not valid"),
   body("password")
     .isStrongPassword()
     .isLength({ min: 8 })
-
-    .withMessage("your Password is not strong , improve your password"),
+    .withMessage("your Password is not strong und must be +8 characters long"),
   errorsValidator,
 ];
 
 exports.login = [
-  body("email").isEmail().withMessage("invalid Email improve your Email"),
+  body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Email is not valid"),
   body("password")
     .isStrongPassword()
     .withMessage("your Password is not strong , improve your password"),
