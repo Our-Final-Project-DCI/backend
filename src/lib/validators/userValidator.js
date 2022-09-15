d9f56cffea81b61eb14399c7c0902aefcb5d773e
alaa nasser <alaanasser749@gmail.com>
2022-09-15 14:59:37 +0200

    controller für route signup

--- src/lib/validators/userValidator.js
+++ src/lib/validators/userValidator.js
@@ -5,14 +5,13 @@ const errorsValidator = require("../middlewares/errorsValidator");
 exports.signup = [
   body("username")
     .exists()
-    .isString()
+
     .isLength({ min: 3 })
     .withMessage("This username must me 3+ characters long "),
   body("email")
     .trim()
     .isEmail()
-    .notEmpty()
-    .normalizeEmail()
+
     .withMessage("Email is not valid"),
   body("password")
     .isStrongPassword()
@@ -21,18 +20,18 @@ exports.signup = [
   errorsValidator,
 ];
 
-exports.login = [
-  body("email")
-    .trim()
-    .isEmail()
-    .notEmpty()
-    .normalizeEmail()
-    .withMessage("Email is not valid"),
-  body("password")
-    .isStrongPassword()
-    .isLength({ min: 8 })
-    .withMessage("your Password is not strong und must be +8 characters long"),
-  errorsValidator,
-];
+// exports.login = [
+//   body("email")
+//     .trim()
+//     .isEmail()
+//     .notEmpty()
+//     .normalizeEmail()
+//     .withMessage("Email is not valid"),
+//   body("password")
+//     .isStrongPassword()
+//     .isLength({ min: 8 })
+//     .withMessage("your Password is not strong und must be +8 characters long"),
+//   errorsValidator,
+// ];
 
 //exports.update = [];
