583a930c8b0baea6e73d214ca12f7748c7f0b217
alaa nasser <alaanasser749@gmail.com>
2022-09-19 11:03:20 +0200

    login-controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,3 +21,34 @@ exports.signup = async (req, res, next) => {
 
   res.status(200).send(newUser);
 };
+
+// 2. login
+/** @type {import("express").RequestHandler} */
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const userFound = await User.findOne().where("email").equals(email);
+
+  if (!userFound) {
+    const error = new Error("This email adress is not FOUND!!");
+    error.status = 401;
+    return next(error);
+  }
+
+  const correctPws = await bcrypt.compare(password, userFound.password);
+  if (!correctPws) {
+    const error = new Error("This password is not correct!!");
+    error.status = 401;
+    return next(error);
+  }
+
+  userFound.token = crypto.randomBytes(64).toString("hex");
+
+  res.cookie("user-token", userFound.token, {
+    maxAge: 100000,
+    sameSite: "strict",
+    httpOnly: true,
+  });
+
+  res.status(200).send(userFound);
+};
