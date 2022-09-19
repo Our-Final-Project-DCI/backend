1227cea88ed69ac8d66ada57fd515598ff49cd03
alaa nasser <alaanasser749@gmail.com>
2022-09-19 16:04:18 +0200

    update user validator

--- src/lib/validators/userValidator.js
+++ src/lib/validators/userValidator.js
@@ -5,30 +5,29 @@ const errorsValidator = require("../middlewares/errorsValidator");
 exports.signup = [
   body("username")
     .exists()
-
+    .trim()
     .isLength({ min: 3 })
     .withMessage("This username must me 3+ characters long "),
-  body("email")
-    .trim()
-    .isEmail()
-
-    .withMessage("Email is not valid"),
+  body("email").trim().isEmail().withMessage("Email is not valid"),
   body("password")
+    .trim()
     .isLength({ min: 6 })
     .withMessage("your Password is not strong und must be +8 characters long"),
   errorsValidator,
 ];
 
 exports.login = [
-  body("email")
-    .trim()
-    .isEmail()
-
-    .withMessage("Email is not valid"),
+  body("email").trim().isEmail().withMessage("Email is not valid"),
   body("password")
+    .trim()
     .isLength({ min: 6 })
     .withMessage("your Password is not strong und must be +6 characters long"),
   errorsValidator,
 ];
 
-//exports.update = [];
+exports.update = [
+  body("gender")
+    .notEmpty()
+    .isIn(["male", "female"])
+    .withMessage("you must select : female or male"),
+];
