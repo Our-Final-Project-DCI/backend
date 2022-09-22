9bdeb55a1a068c576456bd225628c630e3426beb
alaa nasser <alaanasser749@gmail.com>
2022-09-22 10:43:20 +0200

    photo routes

--- src/routes/photosRoutes.js
+++ src/routes/photosRoutes.js
@@ -1,8 +1,29 @@
 const Photo = require("../models/Photo");
-require('express-async-errors')
+require("express-async-errors");
+
 const express = require("express");
+
+const controller = require("../controllers/photosController");
+const validator = require("../lib/validators/photosValidator");
+
+const auth = require("../lib/middlewares/auth");
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
 
 const app = express.Router();
 
-//
+// -> photos/upload-photo
+
+app.post(
+  "/upload-photo",
+  auth,
+  upload.single("file"),
+  validator.uploadPhoto,
+  controller.uploadPhoto
+);
+
+// -> photos/photos
+
+app.get("/", auth, controller.getAllPhotos);
+
 module.exports = app;
